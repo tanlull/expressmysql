9336b8cab07d4579b567e0cf85b51002a934d5d3
Tanya S <tanlull@gmail.com>
2020-06-26 12:26:05 +0700

    Section 2

---
+++ controllers/blogController.js
@@ -0,0 +1,7 @@
+
+
+exports.index = (req, res, next) =>{
+    res.status(200).json({
+      message : 'แสดงรายการ blog'
+    });
+  }
---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const userController = require('../controllers/userController')
+
+/* GET users listing. */
+router.get('/', userController.index);
+
+module.exports = router;
